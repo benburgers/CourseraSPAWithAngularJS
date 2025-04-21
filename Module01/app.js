08d2fecd873ef403982e3330ae2d42c0b960f44e
Ben Burgers <ben@benburgers.eu>
2025-04-21 03:24:22 +0200

    Lecture 6

--- Module01/app.js
+++ Module01/app.js
@@ -2,11 +2,23 @@
     "use strict";
 
     angular
-        .module("myFirstApp", [])
-        .controller("MyFirstController", function ($scope) {
-            $scope.name = "Ben"
-            $scope.sayHello = function () {
-                return "Hello Coursera";
+        .module("NameCalculator", [])
+        .controller("NameCalculatorController", function ($scope) {
+            $scope.name = "";
+            $scope.totalValue = 0;
+
+            $scope.displayNumeric = function () {
+                var totalNameValue = calculateNumericForString($scope.name); // get the total value
+                $scope.totalValue = totalNameValue;
             }
         });
+
+    function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+            totalStringValue += string.charCodeAt(i);
+        }
+        
+        return totalStringValue;
+    }
 })();
